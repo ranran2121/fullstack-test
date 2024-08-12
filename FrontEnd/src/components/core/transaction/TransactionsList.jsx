82450cc4b0c0ad9f1adc36e7ad676928fc102841
francesca <francesca.giammichele@etiqa.it>
2024-08-12 12:11:33 +0200

    feat: add balance and sorting feature

--- FrontEnd/src/components/core/transaction/TransactionsList.jsx
+++ FrontEnd/src/components/core/transaction/TransactionsList.jsx
@@ -1,39 +1,78 @@
-import React from 'react';
-import { Divider, List, Typography } from 'antd';
+import React, { useMemo } from 'react';
+import { Button, Divider, List, Typography } from 'antd';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSortUp, faSortDown, faArrowTrendUp, faArrowTrendDown } from '@fortawesome/free-solid-svg-icons';
 
 import TransactionCard from './TransactionCard';
 
 const { Title } = Typography;
 
-const TransactionsList = ({ data, loadMoreData }) => {
+const TransactionsList = ({ data, loadMoreData, sort, onSortClick }) => {
   if (!data || data.length === 0) {
     return <Title className="mb-0 text-center text-lg font-bold lg:text-xl">No results</Title>;
   }
 
+  const balance = useMemo(
+    () => data.reduce((acc, item) => (item.type === 'income' ? acc + item.amount : acc - item.amount), 0),
+    [data]
+  );
+
   return (
-    <div
-      id="scrollableDiv"
-      style={{
-        height: 'auto',
-        overflow: 'auto'
-      }}
-    >
-      <InfiniteScroll
-        dataLength={data.length}
-        next={loadMoreData}
-        hasMore={loadMoreData !== undefined}
-        endMessage={
-          <Divider plain className="italic">
-            It is all, nothing more{' '}
-          </Divider>
-        }
-        scrollableTarget="scrollableDiv"
+    <>
+      <RecapLine sort={sort} balance={balance} onSortClick={onSortClick} />
+      <div
+        id="scrollableDiv"
+        style={{
+          height: 'auto',
+          overflow: 'auto'
+        }}
       >
-        <List dataSource={data} renderItem={item => <TransactionCard item={item} />} />
-      </InfiniteScroll>
-    </div>
+        <InfiniteScroll
+          dataLength={data.length}
+          next={loadMoreData}
+          hasMore={loadMoreData !== undefined}
+          endMessage={
+            <Divider plain className="italic">
+              It is all, nothing more{' '}
+            </Divider>
+          }
+          scrollableTarget="scrollableDiv"
+        >
+          <List dataSource={data} renderItem={item => <TransactionCard item={item} />} />
+        </InfiniteScroll>
+      </div>
+    </>
   );
 };
 
 export default TransactionsList;
+
+const RecapLine = ({ sort, balance, onSortClick }) => (
+  <div className="flex justify-between">
+    <div className="flex gap-2">
+      <Title level={4}>
+        Balance{' '}
+        <span className={`${balance >= 0 ? 'bg-green-200' : 'bg-orange-200'} rounded-md p-1`}>
+          {' '}
+          {Math.abs(balance)}$
+        </span>
+      </Title>
+      <FontAwesomeIcon
+        icon={balance >= 0 ? faArrowTrendDown : faArrowTrendUp}
+        size="lg"
+        className={`${balance >= 0 ? 'bg-green-200' : 'bg-orange-200'} rounded-md p-1`}
+      />
+    </div>
+    <div className="flex gap-2">
+      <Title level={4}>Sort:</Title>
+      <Button type="button" onClick={onSortClick} className="border-1 border-black p-2">
+        <FontAwesomeIcon
+          icon={sort === 'asc' ? faSortUp : faSortDown}
+          size="lg"
+          className={`${sort === 'asc' ? 'mt-2' : '-mt-2'}`}
+        />
+      </Button>
+    </div>
+  </div>
+);
--- FrontEnd/src/routes/Home.jsx
+++ FrontEnd/src/routes/Home.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useInfiniteQuery } from '@tanstack/react-query';
 import { Result } from 'antd';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -8,8 +8,15 @@ import Api from '../helpers/core/Api';
 import ContentPanel from '../components/core/layout/ContentPanel';
 import TransactionsList from '../components/core/transaction/TransactionsList';
 
-const fetchTransactions = async ({ pageParam = 1, type }) => {
-  const url = type ? `/transactions?type=${type}&page=${pageParam}` : `/transactions?page=${pageParam}`;
+const fetchTransactions = async ({ pageParam = 1, type, sort }) => {
+  let url = `/transactions?page=${pageParam}`;
+  if (type) {
+    url += '&type=' + type;
+  }
+  if (sort) {
+    url += '&sort=' + sort;
+  }
+
   const { data } = await Api.get(url);
   return data;
 };
@@ -17,17 +24,32 @@ const fetchTransactions = async ({ pageParam = 1, type }) => {
 const useQuery = () => new URLSearchParams(useLocation().search);
 
 const Home = () => {
+  const navigate = useNavigate();
   const query = useQuery();
   const type = query.get('type');
+  const sort = query.get('sort') || 'desc';
 
   const { data, error, fetchNextPage, hasNextPage, isLoading } = useInfiniteQuery({
-    queryKey: ['transactions', type],
-    queryFn: context => fetchTransactions({ pageParam: context.pageParam, type }),
+    queryKey: ['transactions', type, sort],
+    queryFn: context => fetchTransactions({ pageParam: context.pageParam, type, sort }),
     getNextPageParam: lastPage => (lastPage.hasMore ? lastPage.nextPage : undefined)
   });
 
   // Flatten the data from pages
   const transactions = data?.pages.flat() || [];
+
+  const onSortClick = () => {
+    const newSort = sort === 'asc' ? 'desc' : 'asc';
+    const searchParams = new URLSearchParams();
+
+    if (type) {
+      searchParams.set('type', type);
+    }
+
+    searchParams.set('sort', newSort);
+
+    navigate(`?${searchParams.toString()}`);
+  };
 
   return (
     <ContentPanel
@@ -40,7 +62,12 @@ const Home = () => {
       {error ? (
         <Result status="warning" title="There are some problems with the operation." />
       ) : (
-        <TransactionsList data={transactions} loadMoreData={hasNextPage ? fetchNextPage : undefined} />
+        <TransactionsList
+          data={transactions}
+          loadMoreData={hasNextPage ? fetchNextPage : undefined}
+          onSortClick={onSortClick}
+          sort={sort || 'desc'}
+        />
       )}
     </ContentPanel>
   );
